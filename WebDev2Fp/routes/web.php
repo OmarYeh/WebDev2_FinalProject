087f7e686be13d6cd84578558060ebf86bbb9580
Hussein Hammoura <47279159+HusseinHammoura@users.noreply.github.com>
2023-05-06 18:47:16 +0300

    HomePage UI (#3)
    * first homepage
    
    * Homepage UI updated. Needs fixing of functionalities.
    
    ---------
    
    Co-authored-by: Hussein <201911372@ua.edu.lb>

--- WebDev2Fp/routes/web.php
+++ WebDev2Fp/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,6 +31,7 @@ Route::middleware('auth')->group(function () {
 Route::get('home', function(){
     return view('home');
 });
+Route::get('search',[HomeController::class,'search'])->name('home.search');
 
 require __DIR__.'/auth.php';
 
