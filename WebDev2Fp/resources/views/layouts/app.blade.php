682a9f2a78f31bf503762b1b5b8d98bda97dfe05
Hussein <201911372@ua.edu.lb>
2023-05-14 20:19:38 +0300

    updated UI to profile and header and related pages

--- WebDev2Fp/resources/views/layouts/app.blade.php
+++ WebDev2Fp/resources/views/layouts/app.blade.php
@@ -39,7 +39,78 @@
             color: inherit;
         }
         /*HOME AND FOOD STYLES*/
-       
+        .homeheader {
+        color:#e55;
+        text-decoration:none;
+        font-size:20px;
+        position: relative;
+        }
+        .homeheader:hover{
+            color:black;
+        }
+        .homeheader:hover:after {
+        content: "";
+        display: block;
+        position: absolute;
+        left: 0;
+        bottom: -2px;
+        width: 100%;
+        height: 2px;
+        background-color: black;
+        transition: all 0.3s ease-in-out;
+        }
+
+        .foodheader {
+        color:#e55;
+        text-decoration:none;
+        font-size:20px;
+        margin-left:5px;
+        position: relative;
+        }
+        .foodheader:hover{
+            color:black;
+        }
+        .foodheader:hover:after {
+        content: "";
+        display: block;
+        position: absolute;
+        left: 0;
+        bottom: -2px;
+        width: 100%;
+        height: 2px;
+        background-color: black;
+        transition: all 0.3s ease-in-out;
+        }
+
+        .cuisineheader {
+        color:#e55;
+        text-decoration:none;
+        font-size:20px;
+        margin-left:5px;
+        position: relative;
+        }
+        .cuisineheader:hover{
+            color:black;
+        }
+        .cuisineheader:hover:after {
+        content: "";
+        display: block;
+        position: absolute;
+        color:black;
+        left: 0;
+        bottom: -2px;
+        width: 100%;
+        height: 2px;
+        background-color: black;
+        transition: all 0.3s ease-in-out;
+        }
+
+        .titles {
+        margin-left: 118%;
+        gap: 28px;
+        display: flex;
+        }
+        
     </style>
     @yield('css')
 
@@ -150,7 +221,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm" style="box-shadow: 0px 4px 18px 3px rgba(238, 85, 85, 0.21) !important;  position: sticky;top: 0;">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -159,7 +230,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <div style="display:flex;align-items: center;gap: 14px;" >
+                        <a href="{{ route('home') }}"><img src="{{asset('storage\images\Foodies.png')}}" style="width:100px;height:30px;"/></a>
+                            <div class="titles">
+                                <a href="{{ route('home') }}" class="homeheader" >Home</a>
+                                <a href="{{ route('searchFood') }}" class="foodheader" >Food</a>
+                                <a href="{{ route('Allcusisnes') }}" class="cuisineheader" >Cuisines</a>
+                            </div>
+                        </div>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -167,23 +245,26 @@
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <li class="nav-item" >
+                                    <a class="nav-link" href="{{ route('login') }}" style="color: #e55;">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <li class="nav-item" >
+                                    <a class="nav-link" href="{{ route('register') }}" style="color: #e55;">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <li class="nav-item dropdown" >
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre style="color: #e55;">
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('profile.edit') }}">
+                                      Profile
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
