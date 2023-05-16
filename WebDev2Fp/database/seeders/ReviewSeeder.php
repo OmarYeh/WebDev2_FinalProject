a0aa81f7dc775c54cea621e8b5b6d6bee9579b54
Hussein <201911372@ua.edu.lb>
2023-05-16 15:33:19 +0300

    fixes to seeders and factories

---
+++ WebDev2Fp/database/seeders/ReviewSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\review;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReviewSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        review::factory(10)->create();
+    }
+}
